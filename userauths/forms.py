2e44911af91e6db366d8a1ee8a51aa0a8651875d
AshrafGit256 <ashrafwalusimbi27@gmail.com>
2025-01-01 20:53:04 +0300

    Finalised model and Database Struction using Django

--- userauths/forms.py
+++ userauths/forms.py
@@ -4,6 +4,11 @@ from userauths.models import User
 
 
 class UserRegisterForm(UserCreationForm):
+    username = forms.CharField(widget=forms.TextInput(attrs={"placeholder":"", "class": "form-control"}))
+    email = forms.EmailField(widget=forms.EmailInput(attrs={"placeholder":"", "class": "form-control"}))
+    password1 = forms.CharField(widget=forms.PasswordInput(attrs={"placeholder":"", "class": "form-control"}))
+    password2 = forms.CharField(widget=forms.PasswordInput(attrs={"placeholder":"", "class": "form-control"}))
+    
     class Meta:
         model = User
         fields = ['username', 'email']
